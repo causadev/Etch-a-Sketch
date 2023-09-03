c51addead397ae9aee98f14b41ecf207d8f2e477
Causa <causadev@proton.me>
2023-09-03 04:49:07 -0500

    Some cool changes :)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Etch-a-Sketch-
+# Etch-a-Sketch
+# This project is part the (https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/etch-a-sketch-project) curriculum.
--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 const container = document.querySelector(".container");
 const gridSizeBtn = document.querySelector("#gridSizeBtn");
 const clearBtn = document.querySelector("#clearBtn");
+const eraserBtn = document.querySelector("#eraserBtn");
+const colorModeBtn = document.querySelector("#colorModeBtn");
+const text = document.querySelector("#text");
 let colorPicker = document.querySelector("#colorPicker");
 let DEFAULT_COLOR = "#000000";
-colorPicker.value = DEFAULT_COLOR;
 let userInput = 16;
+let isDrawing = false;
+colorPicker.value = DEFAULT_COLOR;
+let colorAnterior;
+
+container.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+container.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+eraserBtn.addEventListener("click", () => {
+  colorAnterior = colorPicker.value;
+  // Cambia el color a blanco (#ffffff)
+  colorPicker.value = "#ffffff";
+});
+
+colorModeBtn.addEventListener("click", () => {
+  colorPicker.value = colorAnterior;
+});
 
 clearBtn.addEventListener("click", clearGrid);
 gridSizeBtn.addEventListener("click", () => {
-  userInput = Number(prompt("Enter A Grid Size (max 64x64)"));
-  if (isNaN(userInput)) {
-    alert("Enter a valid input");
-  } else if (userInput == null) {
-    alert("Enter a valid input");
-  } else if (userInput >= 64 || userInput <= 0) {
-    alert("Enter a valid value");
+  while (true) {
+    userInput = Number(prompt("Enter A Grid Size (max 64x64)"));
+
+    if (userInput >= 1 && userInput <= 64) {
+      clearGrid();
+      break;
+    } else if (userInput === "") {
+      clearGrid();
+      break;
+    } else {
+      alert("Enter a valid input between 1 and 64");
+    }
   }
-  clearGrid();
 });
 
 function clearGrid() {
   container.innerHTML = "";
   createGrid();
+  userInput = 16;
 }
 
-colorPicker.addEventListener("change", () => {
-  clearGrid();
+colorPicker.addEventListener("change", (e) => {
+  DEFAULT_COLOR = e.target.value;
 });
 
 function createGrid() {
@@ -34,8 +62,10 @@ function createGrid() {
     gridItem.classList.add("grid-item");
     container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
     container.appendChild(gridItem);
-    gridItem.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = colorPicker.value;
+    gridItem.addEventListener("mousemove", (e) => {
+      if (isDrawing && userInput >= 1 && userInput <= 64) {
+        e.target.style.backgroundColor = colorPicker.value;
+      }
     });
   }
 }
