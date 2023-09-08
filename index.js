b5137429f45b9af8271768f45cd5d6f66f74d3f4
Causa <causadev@proton.me>
2023-09-08 05:57:14 -0500

    New etch-a-sketch

--- index.js
+++ index.js
@@ -1,90 +1,76 @@
 const container = document.querySelector(".container");
-const gridSizeBtn = document.querySelector("#gridSizeBtn");
-const clearBtn = document.querySelector("#clearBtn");
-const eraserBtn = document.querySelector("#eraserBtn");
-const colorModeBtn = document.querySelector("#colorModeBtn");
-const rainbowBtn = document.querySelector("#rainbowBtn");
-const text = document.querySelector("#text");
-let colorPicker = document.querySelector("#colorPicker");
-let DEFAULT_COLOR = "#000000";
-let userInput = 16;
-let isDrawing = false;
-let isRainbow = false;
-colorPicker.value = DEFAULT_COLOR;
-let colorAnterior;
+const color = document.querySelector("input");
+const value = document.querySelector("#value");
+const input = document.querySelector("#sizeSlider");
+const clear = document.querySelector("#clearBtn");
+const rainbow = document.querySelector("#rainbowBtn");
+const erase = document.querySelector("#eraseBtn");
+const colorModeBtn = document.querySelector("#colorBtn");
+let defaultColor = "#000000";
+let mouseDown = false;
+let gridSize = 16;
+let currentMode = "";
+container.onmousedown = () => (mouseDown = true);
+container.onmouseup = () => (mouseDown = false);
+clear.addEventListener("click", clearGrid);
+rainbow.addEventListener("click", rainbowMode);
+erase.addEventListener("click", eraseGrid);
+colorModeBtn.addEventListener("click", colorMode);
 
-rainbowBtn.addEventListener("click", () => {
-  isRainbow = true;
+color.value = defaultColor;
+input.value = gridSize;
+value.textContent = `${gridSize} x ${gridSize}`;
+
+input.addEventListener("input", (event) => {
+  gridSize = event.target.value;
+  value.textContent = `${gridSize} x ${gridSize}`;
+  clearGrid();
 });
 
-container.addEventListener("mousedown", () => {
-  isDrawing = true;
-});
+function clearGrid() {
+  container.innerHTML = "";
+  currentMode = "";
+  createGrid(gridSize);
+}
 
-container.addEventListener("mouseup", () => {
-  isDrawing = false;
-});
+function colorMode() {
+  currentMode = "color";
+}
 
-eraserBtn.addEventListener("click", () => {
-  colorAnterior = colorPicker.value;
-  // Cambia el color a blanco (#ffffff)
-  colorPicker.value = "#ffffff";
-});
-
-colorModeBtn.addEventListener("click", () => {
-  isRainbow = false;
-  colorPicker.value = colorAnterior;
-});
-
-clearBtn.addEventListener("click", clearGrid);
-gridSizeBtn.addEventListener("click", () => {
-  while (true) {
-    userInput = Number(prompt("Enter A Grid Size (max 64x64)"));
-
-    if (userInput >= 1 && userInput <= 64) {
-      clearGrid();
-      break;
-    } else if (userInput === "") {
-      clearGrid();
-      break;
-    } else {
-      alert("Enter a valid input between 1 and 64");
-    }
-  }
-});
+function eraseGrid() {
+  currentMode = "erase";
+}
 
 function rainbowMode() {
+  currentMode = "rainbow";
   const randomR = Math.floor(Math.random() * 256);
   const randomG = Math.floor(Math.random() * 256);
   const randomB = Math.floor(Math.random() * 256);
   return `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
-function clearGrid() {
-  container.innerHTML = "";
-  createGrid();
-  userInput = 16;
-}
-
-colorPicker.addEventListener("change", (e) => {
-  DEFAULT_COLOR = e.target.value;
-});
-
-function createGrid() {
-  for (let i = 0; i < userInput * userInput; i++) {
+function createGrid(size) {
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+    gridItem.addEventListener("mouseover", changeColor);
+    gridItem.addEventListener("mousedown", changeColor);
     container.appendChild(gridItem);
-    gridItem.addEventListener("mousemove", (e) => {
-      if (isDrawing && userInput >= 1 && userInput <= 64) {
-        e.target.style.backgroundColor = colorPicker.value;
-      }
-      if (isDrawing && isRainbow) {
-        e.target.style.backgroundColor = rainbowMode();
-      }
-    });
   }
 }
 
-createGrid();
+function changeColor(event) {
+  if (event.type === "mouseover" && !mouseDown) return;
+  event.target.style.backgroundColor = color.value;
+  if (currentMode === "rainbow") {
+    event.target.style.backgroundColor = rainbowMode();
+  } else if (currentMode === "erase") {
+    event.target.style.backgroundColor = "#ffffff";
+  } else if (currentMode === "color") {
+    event.target.style.backgroundColor = color.value;
+  }
+}
+
+createGrid(gridSize);
